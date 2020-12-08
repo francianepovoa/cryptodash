4767a1801bac8148cdfda2f79037527bad23e4f6
francianepovoa <francianerpovoa@gmail.com>
2020-12-08 12:02:06 +0100

    Add Styled Components

---
+++ src/App/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./welcome";
+
+class App extends Component {
+	render() {
+		return <Welcome />;
+	}
+}
+
+export default App;
