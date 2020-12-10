cc874cd060248476b6a8ef600e388fee615c0635
francianepovoa <francianerpovoa@gmail.com>
2020-12-10 14:38:21 +0100

    Add CoinGrid

---
+++ src/Settings/coinGrid.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import { AppContext } from "../App/AppProvider";
+
+export const CoinGridStyled = styled.div`
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+`;
+
+function CoinGrid() {
+	return (
+		<AppContext.Consumer>
+			{({ coinList }) => (
+				<CoinGridStyled>
+					{Object.keys(coinList).map((coinKey) => (
+						<div> {coinKey} </div>
+					))}
+				</CoinGridStyled>
+			)}
+		</AppContext.Consumer>
+	);
+}
+
+export default CoinGrid;
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -6,12 +6,15 @@ import ConfirmButton from "./ConfirmButton";
 
 import Page from "../Shared/page";
 
+import CoinGrid from "./coinGrid";
+
 class Settings extends Component {
 	render() {
 		return (
 			<Page name='settings'>
 				<Welcome />
 				<ConfirmButton />
+				<CoinGrid />
 			</Page>
 		);
 	}
