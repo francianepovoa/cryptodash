131b687150f7f555aab29756f5614a30a1a81b50
francianepovoa <francianerpovoa@gmail.com>
2020-12-15 13:45:30 +0100

    Dashboard - HighCharts theme

--- src/Dashboard/priceChart.jsx
+++ src/Dashboard/priceChart.jsx
@@ -3,6 +3,8 @@ import highchartsConfig from "./highChartsConfig";
 import { Tile } from "../Shared/tile";
 import { AppContext } from "../App/AppProvider";
 import ReactHighCharts from "react-highcharts";
+import HighchartsTheme from "./highChartsTheme";
+ReactHighCharts.Highcharts.setOptions(HighchartsTheme);
 
 function PriceChart() {
 	return (
